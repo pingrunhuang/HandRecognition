022e74db5082801e75e903f6dabcbf99bc180d8d
Huang Runping <pingrunhuang@gmail.com>
2017-04-16 16:53:39 +0800

    create supportive script

---
+++ dev-support/setup-python-virtual-env.sh
@@ -0,0 +1,10 @@
+# /bin/sh
+
+# TODO 
+# change the virtual_env_dir parameter to be entered from terminal
+
+VIRTUAL_ENV_DIR=cv-env
+
+python3 -m venv $VIRTUAL_ENV_DIR
+
+source $VIRTUAL_ENV_DIR/bin/activate
---
+++ dev-support/sym-link.sh
@@ -0,0 +1,4 @@
+# /bin/sh
+
+mv /usr/local/lib/python3.5/site-packages/cv2.cpython-35m-darwin.so cv2.so
+ln -s /usr/local/lib/python3.5/site-packages/cv2.so ~/dev/HandRecognition/cv-env/lib/python3.5/site-packages/cv2.so
